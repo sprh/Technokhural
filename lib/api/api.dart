08faee031c5c4febb44a0b76ab0094b3de58694d
Sofia <sktimokhina@yandex-team.ru>
2023-05-15 22:18:24 +0300

    Implemented api

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,9 +1,17 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
 import 'package:technokhural_flutter_demo/models/ui_item.dart';
 
 class Api {
   late final uri = Uri.parse('http://158.160.16.111:8000/');
 
   Future<UIItem?> getUI() async {
+    final ui = await get(uri);
+    if (ui.statusCode == 200) {
+      final json = jsonDecode(ui.body) as Map<String, dynamic>;
+      return UIItem.fromJson(json);
+    }
     return null;
   }
 }
